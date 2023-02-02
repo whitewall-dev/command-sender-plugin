ab9c968527defc120747316e3b207fef5cbae34d
ceifa <gabrinelson27@gmail.com>
2023-02-02 09:11:00 -0300

    add components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,17 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useTranslation, Trans } from 'react-i18next';
-import RoutesPath from '../../constants/routes-path';
-
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import settings from '../../config';
-
-import logo from '../../assets/images/svg/blip-balloon.svg';
 import Header from './components/Header';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import Card from '../../components/Card';
 
 const PAGE_ICON = 'plugin';
 const BLANK = '_blank';
 
 const Home = () => {
-    const history = useHistory();
     const { t } = useTranslation();
 
-    const handleNavigation = useCallback(
-        (path, params = {}) => {
-            history.push(path, params);
-        },
-        [history]
-    );
 
     return (
         <div className="ph1 ph4-m ph5-ns pb5">
@@ -30,41 +21,37 @@ const Home = () => {
                 onClick={() => window.open(settings.repositoryUrl, BLANK)}
             />
             <div className="flex flex-column items-center justify-center bp-c-neutral-dark-city f5 h-100 mt4">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p className="tc">
-                    {t('paragraph.homeDescription.part1')}
-                    <br />
-                    <Trans i18nKey="paragraph.homeDescription.part2">
-                        Edit <code>src/pages/Home.js</code> and save to reload.
-                    </Trans>
-                </p>
-                <h5 className="f5 b mt3 mb2">{t('title.exemples')}</h5>
-                <span
-                    className="f6 flex items-center blue no-underline underline-hover pointer"
-                    data-testid="exemple-one"
-                    aria-hidden="true"
-                    onClick={() =>
-                        handleNavigation(RoutesPath.EXAMPLE_PAGE.PATH, {
-                            type: 'storedData'
-                        })
-                    }
-                >
-                    <bds-icon name="file-txt-1" size="x-small" />
-                    {t('link.blipDataExample')}
-                </span>
-                <span
-                    className="f6 flex items-center blue no-underline underline-hover mt1 pointer"
-                    data-testid="exemple-two"
-                    aria-hidden="true"
-                    onClick={() =>
-                        handleNavigation(RoutesPath.EXAMPLE_PAGE.PATH, {
-                            type: 'swrCall'
-                        })
-                    }
-                >
-                    <bds-icon name="file-txt-1" size="x-small" />
-                    {t('link.swrExemple')}
-                </span>
+                <div className="w-50 mt3 mr2 center">
+                    <Card>
+                        <div className="flex flex-column g7-s">
+                            <div>
+                                <Input
+                                    name="to"
+                                    label="To"
+                                    placeholder="postmaster@msging.net"
+                                />
+                            </div>
+                            <div className="mt2">
+                                <Input
+                                    name="method"
+                                    label="Method"
+                                    placeholder="get"
+                                />
+                            </div>
+                            <div className="mt2">
+                                <Input
+                                    name="uri"
+                                    label="Uri"
+                                    placeholder="/contacts"
+                                />
+                            </div>
+                            <div className="mt2 flex justify-end">
+                                <Button
+                                    text="Send" />
+                            </div>    
+                        </div>
+                    </Card>
+                </div>
             </div>
         </div>
     );
